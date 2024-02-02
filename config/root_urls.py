83666eeb9df64a8414dd953efdc8c8b0c096d6dc
SaJH <wogur981208@gmail.com>
2024-02-02 10:00:00 +0900

    Init: 게시글 관련 앱 생성 #8
    Related to #8

--- config/root_urls.py
+++ config/root_urls.py
@@ -7,8 +7,15 @@ urlpatterns = [
     # Admin
     path("admin", admin.site.urls),
     # API
-    path("api/v1/auth/", include(("jurin.authentication.urls", "api-auth"))),
-    path("api/v1/users/", include(("jurin.users.urls", "api-users"))),
+    path("api/v1/auth", include(("jurin.authentication.urls", "api-auth"))),
+    path("students/api/v1/users", include(("jurin.users.students.urls", "students-api-users"))),
+    path("teachers/api/v1/users", include(("jurin.users.teachers.urls", "teachers-api-users"))),
+    path("students/api/v1/channels", include(("jurin.channels.students.urls", "students-api-channels"))),
+    path("teachers/api/v1/channels", include(("jurin.channels.teachers.urls", "teachers-api-channels"))),
+    path("students/api/v1/channels/<int:channel_id>/posts", include(("jurin.posts.students.urls", "students-api-posts"))),
+    path("teachers/api/v1/channels/<int:channel_id>/posts", include(("jurin.posts.teachers.urls", "teachers-api-posts"))),
+    path("students/api/v1/channels/<int:channel_id>/items", include(("jurin.items.students.urls", "students-api-items"))),
+    path("teachers/api/v1/channels/<int:channel_id>/items", include(("jurin.items.teachers.urls", "teachers-api-items"))),
 ]
 
 from config.settings.debug_toolbar.setup import DebugToolbarSetup  # noqa

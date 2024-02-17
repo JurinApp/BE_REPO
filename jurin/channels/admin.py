41ee813a4ec9089ef451ea291f146be186cf20a9
SaJH <wogur981208@gmail.com>
2024-02-17 16:50:48 +0900

    Feat: 채널 객체 관리자에 추가
    - 임시적으로 시장 시작 시간, 종료시간을 자유롭게 조정하기 위함

---
+++ jurin/channels/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from jurin.channels.models import Channel
+
+admin.site.register(Channel)
--- jurin/users/admin.py
+++ jurin/users/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 
 from jurin.users.models import VerificationCode
 
-# @TODO: 어드민 페이지 구축 전 인증코드 생성을 위한 임시 어드민 페이지
 admin.site.register(VerificationCode)

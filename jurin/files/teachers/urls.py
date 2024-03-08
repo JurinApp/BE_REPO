999f894ec2c06707877a741e0b67cf7c1fc34368
SaJH <wogur981208@gmail.com>
2024-03-08 20:30:05 +0900

    Refact: 파일 관련 모듈 리팩토링

--- jurin/files/teachers/urls.py
+++ jurin/files/teachers/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from jurin.files.teachers.apis import FileUploadAPI
 
 urlpatterns = [
-    path("/upload", FileUploadAPI.as_view(), name="file-upload"),
+    path("/upload", FileUploadAPI.as_view(), name="teacher_file_upload"),
 ]
